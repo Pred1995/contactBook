ee7b838d64ceb2b4dfa9a7cb6de0b18df19cda9d
н НН <beleberdek@yandex.ru>
2020-11-28 16:33:12 +0300

    3 commit

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ import router from "./router";
 import store from "./store";
 import Vuelidate from "vuelidate";
 import VueTelInput from "vue-tel-input";
-import UUID from "vue-uuid";
 
 Vue.use(Vuelidate);
 Vue.use(VueTelInput);
-Vue.use(UUID);
 
 Vue.config.productionTip = false;
 
